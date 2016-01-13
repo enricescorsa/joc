7cd8cd4d1e86ed5d274475f306ab828df7a01af5
Enric Escorsa O'Callaghan <enricescorsa@users.noreply.github.com>
2016-01-13 12:17:02 +0100

    Create README.py

---
+++ README.py
@@ -0,0 +1,48 @@
+# joc
+joc introducció python per a nens
+
+#proba
+
+print('Hola!')
+print('com et dius? (escriu el teu nom)')
+myName=raw_input('')
+print('Hola, '+myName)
+print('El número de lletres que té el teu nom és:')
+print(len(myName))
+print('quants anys tens?')
+myAge=input()
+print('Molt bé. Sabies que quan hagin passat 10 anys tindràs '+str(int(myAge)+10)+' anys?')
+raw_input('')
+print('OK. Com veus, sé moltes coses de tu '+myName+'!')
+print('Una altra pregunta: quants jocs de la play tens?')
+numCats=raw_input()
+try:
+    if int(numCats)>=4:
+        print ('Caram! quants jocs. Que mimat! Segur que et vicies molta estona!')
+    else:
+        print('que poquets! Molt bé, així no et distreus tant i estudies més')
+except ValueError:
+    print('això no és cap número xavalet!')
+import random
+print('Una altra pregunta; '+myName+', a veure si endevines un número de 1 al 20 que estic pensant...')
+secretNumber=random.randint(1,20)
+
+for guessesTaken in range(1, 7):
+    print('Digues quin creus que és?')
+    guess=int(input())
+    if guess>secretNumber:
+        print('massa gran')
+    elif guess<secretNumber:
+        print ('massa petit')
+    else:
+        break
+if guess==secretNumber:
+    print('Molt bé '+myName+'! Ho has endevinat en '+str(guessesTaken)+' intents.')
+else:
+    print('No. Ho sento, el número era '+str(secretNumber)) 
+
+
+print('Adéu '+myName+'!')
+
+
+
